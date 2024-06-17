1c84a30c27e97ebfa81d15d16f9e850de08f29e0
Benjamin Krueger <konto@blk8.de>
2024-06-17 22:09:09 +0200

    Add Privacy Policy page and link on layout
    A new Privacy Policy page has been added to kringlecraft. This includes creating a new HTML template for the privacy policy content and adding a routing function in the view. Additionally, a hyperlink to the Privacy Policy is included in the shared layout template, replacing the previous placeholder text.

--- kringlecraft/views/home_views.py
+++ kringlecraft/views/home_views.py
@@ -3,6 +3,13 @@ import flask
 blueprint = flask.Blueprint('home', __name__, template_folder='templates')
 
 
+# Show index page
 @blueprint.route('/')
 def index():
     return flask.render_template('home/index.html')
+
+
+# Show privacy policy
+@blueprint.route('/privacy')
+def privacy():
+    return flask.render_template('home/privacy.html')
